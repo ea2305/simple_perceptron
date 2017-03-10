60ba2aa396b2245cb13029028146d8c81e5e5255
Elihu <ea_2305@hotmail.com>
2017-03-10 11:11:58 -0600

    Rewrite elements and write the description

---
+++ README.md
@@ -0,0 +1,23 @@
+# Webpack React boilerplate
+
+Basic implementation of webpack with reactjs
+
+## Requirements
+* Node lts (6.9.1)
+
+## Configuration
+* Install project
+
+```[js]
+npm install
+```
+
+* Test project
+```[js]
+npm start
+```
+
+* build project
+```[js]
+npm run build
+```
--- app/boostrap.jsx
+++ app/boostrap.jsx
@@ -1,12 +1,13 @@
+//Import elements 
 import React,{Component} from 'react';
 import {render} from 'react-dom';
 
 const target = document.getElementById( 'app' );
 
+
 class App extends Component{
 	render(){
-		console.log('ok')
-		return <h1>Hola mundo</h1>;
+		return <h1>Hola mundo!!</h1>;
 	}
 }
 
